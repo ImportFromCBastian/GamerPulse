cbf2f472317dc6191813d715556e0a46c76f0cd1
ImportFromCBastian <sebshndz2001@gmail.com>
2023-04-10 03:29:09 -0300

    ended poin a

--- Entrega1/index.php
+++ Entrega1/index.php
@@ -10,31 +10,42 @@
   <header> <img class="logo" src="./images/GamerPulse.jpg" alt="logo"></header>
 
   <form action="index.php" method="get">
-    <input class ="inputs" type="text" name="inputName"  placeholder="Nombre">
-    <input class ="inputs" type="text" name="inputGender"  placeholder="G&eacute;nero">
-    <input class ="inputs" type="text" name="inputConsole"  placeholder="Plataforma">
+    <div class="form">
+        <input class ="inputFilter" type="text" name="inputName"  placeholder="Nombre">
+        <input class ="inputFilter" type="text" name="inputGender"  placeholder="G&eacute;nero">
+        <input class ="inputFilter" type="text" name="inputConsole"  placeholder="Plataforma">
+        <span>      
+          <input class ="inputs" type="submit" name="filter" value="Filtrar">
+          <input class ="inputs add" type="submit" name="addGame" value="Agregar Juego" action="altaJuego.php">
+        </span>
+      </div>
+
+
     </form>
     <div class="divBox">
       
         <div class ="staticData">
-          <p ><a href="https://articulo.mercadolibre.com.ar/MLA-1291698023-juego-new-super-mario-2-3ds-_JM#position=6&search_layout=stack&type=item&tracking_id=6b9e8ebc-43f2-46b4-a764-9a2cf34cce41" target="_blank"><img class="logo" src="./images/GamerPulse.jpg" alt="logo"></a></p>
+          <p ><a href="https://articulo.mercadolibre.com.ar/MLA-1291698023-juego-new-super-mario-2-3ds-_JM#position=6&search_layout=stack&type=item&tracking_id=6b9e8ebc-43f2-46b4-a764-9a2cf34cce41" target="_blank"><img class="img_in_box" src="./images/NewSuperMarioBros2.png" alt="logo"></a></p>
           <p>New Super Mario Bros 2 - 3DS</p>
           <p>Plataforma</p>
-          <p>Juego nuevo de la saga super mario</p>
+          <Strong><p>Juego nuevo de la saga super mario</p></Strong>
         </div>
       
         <div class ="staticData">
-          <p><a href="https://articulo.mercadolibre.com.ar/MLA-1291698023-juego-new-super-mario-2-3ds-_JM#position=6&search_layout=stack&type=item&tracking_id=6b9e8ebc-43f2-46b4-a764-9a2cf34cce41" target="_blank"><img class="logo" src="./images/GamerPulse.jpg" alt="logo"></a></p>
-          <p>New Super Mario Bros 2 - 3DS</p> 
-          <p>Plataforma</p>
-          <p>Juego nuevo de la saga super mario</p>
+          <p><a href="https://store.steampowered.com/app/960090/Bloons_TD_6/" target="_blank"><img class="img_in_box" src="./images/BloonsTD6.jpg" alt="logo"></a></p>
+          <p>Bloons TD6 - PC/Mobile</p> 
+          <p>Tower Defense</p>
+          <Strong><p>Crea la defensa perfecta</p>
+          <p>con una combinación increíble de </p>
+          <p>torres moneriles. </p></Strong>
         </div>
 
         <div class ="staticData">
-          <p><a href="https://articulo.mercadolibre.com.ar/MLA-1291698023-juego-new-super-mario-2-3ds-_JM#position=6&search_layout=stack&type=item&tracking_id=6b9e8ebc-43f2-46b4-a764-9a2cf34cce41" target="_blank"><img class="logo" src="./images/GamerPulse.jpg" alt="logo"></a></p>
-          <p>New Super Mario Bros 2 - 3DS</p> 
-          <p>Plataforma</p>
-          <p>Juego nuevo de la saga super mario</p>
+          <p><a href="https://www.gog.com/game/hollow_knight?pp=7db6e65d0713f435da1a90d06da2c98465764f38" target="_blank"><img class="img_in_box" src="./images/HollowKnight.jpg" alt="logo"></a></p>
+          <p>Hollow Knight - PC/Nintendo Switch</p> 
+          <p>Aventura</p>
+          <Strong><p>Juego Clasico de acci&oacute;n 2D </p>
+            <p>en un vasto mundo interconectado.</p></Strong>
         </div>
       
   </div>
