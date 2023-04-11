2458bca4a4be989293a99cee55c10f171025da3b
GregoPonce <129854601+GregoPonce@users.noreply.github.com>
2023-04-11 13:28:39 -0300

    Update altaJuego.php

--- Entrega1/altaJuego.php
+++ Entrega1/altaJuego.php
@@ -1,1 +1,31 @@
-<?php echo '<p>Hola Mundo</p>'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alta Juego</title>
+</head>
+<body>
+    <form action="altaJuego.php" method="get">
+        <label for ="nombre"> Nombre:</label>
+        <input type ="text" id ="nombre" name="nombre"> <br>
+
+        <label for ="img"> Imagen:</label>
+        <input type ="text" id ="img" name="img"> <br>
+
+        <label for ="descripcion"> Descripcion:</label>
+        <input type ="text" id ="descripcion" name="descripcion"> <br>
+
+        <label for ="plataforma"> Plataforma:</label>
+        <input type ="text" id ="plataforma" name="plataforma"> <br>
+
+        <label for ="url"> URL:</label>
+        <input type ="text" id ="url" name="url"> <br>
+
+        <label for ="genero"> Genero:</label>
+        <input type ="text" id ="genero" name="genero"> <br>
+
+    </form>
+</body>
+</html>
