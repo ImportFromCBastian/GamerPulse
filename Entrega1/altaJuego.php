9c89f9ec6703af156e7a079bd37fb0a65a501519
ImportFromCBastian <sebshndz2001@gmail.com>
2023-04-18 12:45:08 -0300

    Styled

--- Entrega1/altaJuego.php
+++ Entrega1/altaJuego.php
@@ -5,21 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alta Juego</title>
-    <link rel="stylesheet" href="estilo.css"></link>
+    <link rel="stylesheet" href="estilos.css"></link>
 </head>
 <body>
-    <header> <img class="logo" src=./images/GamerPulse.jpg" alt="logo"></header>
+    <header> <img class="logo" src="./images/GamerPulse.jpg" alt="logo"></header>
     <div class="alta" >
+      <div>
+        <h3>Agregar Juego</h3>
+      </div>
         <form  action="altaJuego.php" method="get" id="formulario">
 
 
-                <input class="inputs" type ="text" placeholder="Nombre del Juego" id ="nombre" name="nombre">
+                <input class="inputFilter" type ="text" placeholder="Nombre del Juego" id ="nombre" name="nombre">
 
-                <input class ="inputs" type ="text" placeholder="Imagen de la caratula " id ="img" name="img">
+                <input class ="inputFilter" type ="text" placeholder="Imagen de la caratula " id ="img" name="img">
 
-                <input class="inputs" type ="text" placeholder="Descripcion" id="descripcion" name="descripcion">
+                <input class="inputFilter" type ="text" placeholder="Descripcion" id="descripcion" name="descripcion">
 
-                <select class="inputs"id="plataforma" name="plataforma">
+                <input class="inputFilter" type ="text" placeholder="URL del juego" id ="url" name="url">
+                <select class="inputFilter"id="plataforma" name="plataforma">
                     <option value="defaultPlat">Seleccione una plataforma</option>
                     <option value="pc">PC</option>
                     <option value="ps4">PlayStation 4</option>
@@ -27,16 +31,18 @@
                     <option value="switch">Nintendo Switch</option>
                 </select>
 
-           <input class="inputs" type ="text" placeholder="URL del juego" id ="url" name="url">
 
-            <select class="inputs" id="genero" name="genero">
+            <select class="inputFilter" id="genero" name="genero">
               <option value="defaultGen">Seleccione un g&eacute;nero</option>
               <option value="accion">Accion</option>
               <option value="aventura">Aventura</option>
               <option value="fantasia">Fantasia</option>
               <option value="peleas">Peleas</option>
             </select>
+          <span>
             <input class="inputs" type='submit' value='Validar'>
+            <input class="inputs" type="reset" value="Reset">
+          </span>
         </form>
     </div> 
 
