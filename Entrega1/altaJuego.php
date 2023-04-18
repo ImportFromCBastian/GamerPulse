72b3b76541c92ee642ba9980ab9088e916a4500c
Sebastian Hernandez <107598174+ImportFromCBastian@users.noreply.github.com>
2023-04-18 08:21:24 -0300

    Add files via upload

--- Entrega1/altaJuego.php
+++ Entrega1/altaJuego.php
@@ -5,27 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alta Juego</title>
+    <link rel="stylesheet" href="estilo.css"></link>
 </head>
 <body>
+    <header> <img class="logo" src=./images/GamerPulse.jpg" alt="logo"></header>
+
     <form action="altaJuego.php" method="get">
         <label for ="nombre"> Nombre:</label>
         <input type ="text" id ="nombre" name="nombre"> <br>
-
         <label for ="img"> Imagen:</label>
         <input type ="text" id ="img" name="img"> <br>
-
         <label for ="descripcion"> Descripcion:</label>
         <input type ="text" id ="descripcion" name="descripcion"> <br>
-
         <label for ="plataforma"> Plataforma:</label>
         <input type ="text" id ="plataforma" name="plataforma"> <br>
-
         <label for ="url"> URL:</label>
         <input type ="text" id ="url" name="url"> <br>
-
         <label for ="genero"> Genero:</label>
         <input type ="text" id ="genero" name="genero"> <br>
+    </form> 
+    <footer>
+      <h3>Participantes:</h3>
+      <ul>
+        <strong><li><a href="https://github.com/GregoPonce">Gregorio Ponce 2023</a></li></strong>
+        <strong><li><a href="https://github.com/ImportFromCBastian">Sebastian Hernandez 2023</a></li></strong>
+      </ul>
+    </footer>
 
-    </form>
 </body>
 </html>
