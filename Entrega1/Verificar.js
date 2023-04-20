efc441172730b7e5f2dd3e3bf87b0b15ad76eaae
GregoPonce <129854601+GregoPonce@users.noreply.github.com>
2023-04-20 08:45:28 -0300

    Update Verificar.js

--- Entrega1/Verificar.js
+++ Entrega1/Verificar.js
@@ -1,11 +1,26 @@
-function verificador(){
-    if (form.getElementById("nombre").value!== null) 
-    && (form.getElementById("img").value!== null) 
-    && ((form.getElementById("plataforma").value!== null) || (form.getElementById("plataforma").Lenght>255)) 
-    && ((form.getElementById("plataforma").value!=="PS5")||(form.plataforma.get()!=="Wii"))
-    && (form.getElementById("url").Lenght()>80)
-    &&((form.getElementById("genero").value==="Accion") ||(form.getElementById("genero").value==="Aventura")) {
-        return true;
+function verificador(event){ 
+    var alerta="";
+    if (document.getElementById("nombre").value === ""){
+      alerta += "El nombre es obligatorio. ";
+    } 
+    if (document.getElementById("img").value.length === 0) {
+      alerta += "Seleccione una imagen. ";
+    }
+    if ((document.getElementById("descripcion").value == null) || (document.getElementById("descripcion").length>255)) {
+      alerta += "El campo descripcion es incorrecto. ";
+    }
+    if (!["pc", "ps4", "xbox", "switch"].includes(document.getElementById('plataforma').value)) {
+      alerta += "Seleccione la plataforma. ";
+    }
+    if (document.getElementById("url").length>80){
+      alerta += "El campo url es incorrecto. ";
+    }
+    if (!["accion", "aventura", "fantasia", "peleas"].includes(document.getElementById('genero').value)) {
+      alerta += "Advertencia: no eligió género ";
     }
 
-}
+   document.getElementById('error').innerHTML=alerta;
+    if (alerta !==''){
+      event.preventDefault();   
+    }   
+    }
