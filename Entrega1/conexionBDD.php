8b31a5ab71921e79095eb4b85ca2a68d33586378
ImportFromCBastian <sebshndz2001@gmail.com>
2023-04-25 09:22:09 -0300

    Added conexion to DB

---
+++ Entrega1/conexionBDD.php
@@ -0,0 +1,14 @@
+<?php
+function conectar(){ 
+  $conexion = new mySqli("localhost","root","","gamerpulse");
+
+  if($conexion->connect_errno){
+    echo "Fallo al conectar con SQL: " .$conexion->connect_errno;
+  }
+  return $conexion;
+}
+
+
+
+
+?>
