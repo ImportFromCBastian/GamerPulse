7dea58db41b79f4b4ee6f2fa2518a74d32eeabd3
GregoPonce <129854601+GregoPonce@users.noreply.github.com>
2023-04-26 16:07:47 -0300

    Update Verificar.js

--- Entrega1/Verificar.js
+++ Entrega1/Verificar.js
@@ -1,26 +1,63 @@
-function verificador(event){ 
-    var alerta="";
+function verificador(){    
+  var alerta="";
     if (document.getElementById("nombre").value === ""){
       alerta += "El nombre es obligatorio. ";
     } 
     if (document.getElementById("img").value.length === 0) {
       alerta += "Seleccione una imagen. ";
     }
-    if ((document.getElementById("descripcion").value == null) || (document.getElementById("descripcion").length>255)) {
+    if ((document.getElementById("descripcion").value.length>255)) {
       alerta += "El campo descripcion es incorrecto. ";
     }
-    if (!["pc", "ps4", "xbox", "switch"].includes(document.getElementById('plataforma').value)) {
-      alerta += "Seleccione la plataforma. ";
-    }
-    if (document.getElementById("url").length>80){
+    if (document.getElementById("url").value.length>80){
       alerta += "El campo url es incorrecto. ";
+    } 
+     if ( document.getElementById('plataforma').value== -1) {
+      alerta += "Seleccione una plataforma válida. ";
     }
-    if (!["accion", "aventura", "fantasia", "peleas"].includes(document.getElementById('genero').value)) {
-      alerta += "Advertencia: no eligió género ";
+    if (document.getElementById('genero').value == -1) {
+      alerta += "Seleccione un genero válido. ";
     }
+    
+  if (alerta !==''){
+    document.getElementById('error').innerHTML=alerta; }
+    else {
+      document.getElementById('error').innerHTML="valido";
+     var dataForm = new FormData();
 
-   document.getElementById('error').innerHTML=alerta;
-    if (alerta !==''){
-      event.preventDefault();   
-    }   
+     dataForm.append('nombre', document.getElementById('nombre').value);
+
+     dataForm.append('descripcion', document.getElementById('descripcion').value);
+
+     dataForm.append('url', document.getElementById('url').value);
+
+     dataForm.append('plataforma', document.getElementById('plataforma').value);
+
+     dataForm.append('genero', document.getElementById('genero').value);
+
+     var inputFile = document.getElementById('img');
+     dataForm.append('img', inputFile.files[0]);
+
+     fetch('addJuego.php', {
+       method: 'POST',
+       body: dataForm
+     })
+    //  .then(function(response) {
+    //     if(response) {
+    //         return response.text()
+    //     } else {
+    //         throw "Error en la llamada Ajax";
+    //     } 
+    //  })
+    //  .then(function(texto) {
+    //   if(texto=='ok')
+      
+    //     console.log(texto);
+    //     //
+    //  })
+    //  .catch(function(err) {
+    //     console.log(err);
+    //  });
+     
+   }
     }
