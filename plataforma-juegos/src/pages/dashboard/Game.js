29ea8ef3cb53ea3d36398d1baad5504814c42d93
GregoPonce <gregorio.ponce213612@alumnos.info.unlp.edu.argit config --global user.email>
2023-06-29 19:05:05 -0300

    AGUANTE

--- plataforma-juegos/src/pages/dashboard/Game.js
+++ plataforma-juegos/src/pages/dashboard/Game.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../assets/css/generalStyles.css';
+import axios from 'axios';
+import endpoints from '../../config/endpoints';
+
 
 
 const Game = (props)=>{
+
+  const [responseGender,setResponseGender] = useState();
+  const [responsePlatform,setResponsePlatform] = useState();
+
+  useEffect(() =>{
+    const buscar=async (id_genero,id_plataforma)=>{
+      try{
+        let fetchGender = await axios.get(`${endpoints.gender.fetch}${id_genero}`);
+        let fetchPlatform = await axios.get(`${endpoints.platform.fetch}${id_plataforma}`);
+        setResponseGender(fetchGender.data.nombre);
+        setResponsePlatform(fetchPlatform.data.nombre);
+      }catch(e){
+        console.log(e);
+      }
+    }
+    buscar(props.elem.id_genero,props.elem.id_plataforma);
+  },[]);
+
   return(
     <>
       <div key={props.elem.id} className='juegos'>
@@ -11,6 +32,12 @@ const Game = (props)=>{
           <img src={`data:${props.elem.tipo_imagen};base64, ${props.elem.imagen}`} />
           <br></br>
           <p>{props.elem.descripcion}</p>
+          <br></br>
+          <p>{props.elem.url}</p>
+          <br></br>
+          <p>{responseGender}</p>
+          <br></br>
+          <p>{responsePlatform}</p>
         </div>
     </>
   )
