71223a126f17096c9b73fd647c1b3665634439fb
GregoPonce <gregorio.ponce213612@alumnos.info.unlp.edu.ar>
2023-06-29 13:21:15 -0300

    boqota

---
+++ plataforma-juegos/src/pages/dashboard/DropDown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dropdown = ({ name, placeholder, value, options, onChange }) => {
+  return (
+    <select name={name} value={value} onChange={onChange}>
+      <option value=''>{placeholder}</option>
+      {options.map((option) => (
+        <option key={option.id} value={option.id}>{option.nombre}</option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
